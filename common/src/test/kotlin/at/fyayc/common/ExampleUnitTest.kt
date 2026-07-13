72ec5d3ab4b0c6f56b375e64dee0ee56fb5993ad
Bernhard Posselt <bernhard@posselt.at>
2026-07-13 12:16:28 +0200

    try to fix backend tests

---
+++ common/src/test/kotlin/at/fyayc/common/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package at.fyayc.common
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ExampleUnitTest {
+    @Test
+    fun name() {
+        Assertions.assertEquals(0, 1)
+    }
+}
